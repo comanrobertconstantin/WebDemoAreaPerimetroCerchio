700d52c4aa03612a2acaf3159abc0b9d5d7d334c
Robert Constantin Coman <coman.robert.constantin@gmail.com>
2020-03-23 21:36:44 +0100

    fine codice e progetto

---
+++ WebDemoAreaPerimetroCerchio/WebDemoAreaPerimetroCerchio/Controllers/OperationsController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebDemoAreaPerimetroCerchio.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    public class OperationsController : Controller
+    {
+
+        // GET: api/<controller>
+        [HttpGet]
+        public double GetPerimeter(double raggio)
+        {
+            return 2 * Math.PI * raggio;
+        }
+
+
+        // GET: api/<controller>
+        [HttpGet]
+        public double GetArea(double raggio)
+        {
+            return raggio * raggio * Math.PI;
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
